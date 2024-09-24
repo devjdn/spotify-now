e971674a82bd00db0aa2755e4b1a5ad37e79e3f3
Jayden Priestley <153106449+devjdn@users.noreply.github.com>
2024-09-24 14:06:56 +0000

    should have fixed an issue with scroll btns

--- site/components/scroll-btns.tsx
+++ site/components/scroll-btns.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import  { useEffect, useState } from 'react';
+import  { useEffect, useState, useCallback } from 'react';
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface ScrollBtnProps {
@@ -9,33 +9,32 @@ interface ScrollBtnProps {
 
 export const ScrollBtns = ({ scrollTargetSelector }: ScrollBtnProps) => {
     const [scrollContainer, setScrollContainer] = useState<HTMLUListElement | null>(null);
+    const [isScrolling, setIsScrolling] = useState(false);
 
     useEffect(() => {
         const container = document.querySelector(scrollTargetSelector) as HTMLUListElement | null;
         setScrollContainer(container);
     }, [scrollTargetSelector]);
 
-    const scrollLeft = () => {
-        if (scrollContainer) {
+    const scroll = useCallback((direction: 'left' | 'right') => {
+        if(scrollContainer && !isScrolling) {
+            setIsScrolling(true);
             scrollContainer.scrollBy({
-                left: -320,
+                left: direction === 'left' ? -320 : 320,
                 behavior: 'smooth',
             });
+
+            setTimeout(() => setIsScrolling(false), 400)
         }
-    }
-    const scrollRight = () => {
-        if (scrollContainer) {
-            scrollContainer.scrollBy({
-                left: 320,
-                behavior: 'smooth',
-            });
-        }
-    }
+    }, [scrollContainer, isScrolling]);
+
+    const scrollRight = useCallback(() => scroll('right'), [scroll]);
+    const scrollLeft = useCallback(() => scroll('left'), [scroll]);
 
     return(
         <div className="scroll-btn-group">
-            <button className="scroll-btn scroll-left" onClick={scrollLeft}><ChevronLeft/></button>
-            <button className="scroll-btn scroll-right" onClick={scrollRight}><ChevronRight/></button>
+            <button className="scroll-btn scroll-left" onClick={scrollLeft} disabled={isScrolling}><ChevronLeft/></button>
+            <button className="scroll-btn scroll-right" onClick={scrollRight} disabled={isScrolling}><ChevronRight/></button>
         </div>
     );
 }
