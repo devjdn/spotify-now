e56c8bfb308af160630e6ec901a11d64d7877be9
Jayden Priestley <153106449+devjdn@users.noreply.github.com>
2024-10-28 00:31:16 +0000

    Home api calls are both back on the server, performance issues were too much. Found a way to have the shelf component in the way apple music do it, and how tom recommended. Need to fix the scroll buttons.

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -3,14 +3,13 @@ import FeaturedPlaylists from "@/components/spotify/featured-playlists";
 import NewReleases from "@/components/spotify/new-releases";
 
 export default async function Home() {
-  const accessToken = await getCachedAccessToken();
 
   return (
       <>
         <section className="intro">
           <h1>Home</h1>
         </section>
-        <NewReleases accessToken={accessToken}/>
+        <NewReleases/>
         <FeaturedPlaylists/>
       </>
   );
--- site/components/spotify/new-releases.tsx
+++ site/components/spotify/new-releases.tsx
@@ -1,88 +1,20 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import { fetchNewReleases } from "@/api/home-api-calls";
 import { SpotifyNewReleases } from "@/lib/global";
 import { ShelfContainer, ShelfHeader, ShelfScroll, ShelfUl, ShelfLi } from "../shelf/shelf";
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+// import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ScrollBtns } from '../buttons/scroll-btns';
 
-const NewReleases = ({accessToken}: {accessToken: string | null}) => {
-
-    const [newReleases, setNewReleases] = useState<any[]>([]);
-    const [currentPage, setCurrentPage] = useState(0);
-    const [limit, setLimit] = useState<number | null>(null);
-
-    useEffect(() => {
-        const updateLimit = () => {
-            const screenWidth = window.innerWidth;
-
-            switch (true) {
-                case (screenWidth > 1500):
-                    setLimit(12);
-                    break;
-                case (screenWidth > 1200):
-                    setLimit(10);
-                    break;
-                case (screenWidth > 900):
-                    setLimit(8);
-                    break;
-                case (screenWidth > 669):
-                    setLimit(6);
-                    break;
-                default:
-                    setLimit(36);
-            }
-        };
-
-        updateLimit();
-
-        window.addEventListener('resize', updateLimit);
-
-        return () => {
-            window.removeEventListener('resize', updateLimit);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (limit === null) return;
-
-        const loadNewReleases = async () => {
-            try {
-                const data: SpotifyNewReleases = await fetchNewReleases(currentPage * limit, {accessToken, limit});
-                setNewReleases(data.items);
-                console.log(data)
-            } catch (error) {
-                console.error("Failed to load new releases:", error);
-            }
-        }
-        loadNewReleases();
-    }, [currentPage, limit, accessToken]);
-
-    const nextPage = () => {
-        setCurrentPage(prevPage => prevPage + 1)
-    }
-
-    const previousPage = () => {
-        if (currentPage > 0) {
-            setCurrentPage(prevPage => prevPage - 1);
-        }
-    }
-
-    if (limit === null) {
-        return null;
-    }
-
+const NewReleases = async () => {
+    const newReleases: SpotifyNewReleases = await fetchNewReleases();
     return(
         <section className="new-releases" id="new-drops">
             <ShelfContainer>
                 <ShelfHeader shelfTitle="New Releases">
-                    <div className="page-btns">
-                        <button className="page-btn" onClick={previousPage} disabled={currentPage === 0}><ChevronLeft/></button><button className="page-btn" onClick={nextPage} disabled={currentPage === 3}><ChevronRight/></button>
-                    </div>
+                    <ScrollBtns scrollTargetSelector="ul.shelf-ul"/>
                 </ShelfHeader>
                 <ShelfScroll>
                 <ShelfUl>
-                    {newReleases.map((items, itemIndex) => (
+                    {newReleases.items.map((items, itemIndex) => (
                         <ShelfLi key={itemIndex} itemArtwork={items.images[0]?.url} itemTitle={items.name} itemDetails={items.artists[0].name} titleId={`/album/${items.id}`} detailsId={`/artist/${items.artists[0].id}`}/>
                     ))}
                 </ShelfUl>
